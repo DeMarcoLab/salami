8628f1bda8f976f32a48164e73ec403c3b7392fb
Patrick Cleeve <patrick.cleeve@monash.edu>
2023-02-19 18:33:53 +1100

    salami speedrun any%

--- salami/ui/SalamiUI.py
+++ salami/ui/SalamiUI.py
@@ -3,10 +3,11 @@ import os
 
 import napari
 import napari.utils.notifications
-from fibsem.structures import BeamType
-from fibsem.ui import utils as ui_utils
+from fibsem.structures import ImageSettings, FibsemPatternSettings, FibsemImage
 from fibsem.ui.FibsemImageSettingsWidget import FibsemImageSettingsWidget
 from fibsem.ui.FibsemMovementWidget import FibsemMovementWidget
+from fibsem.ui.FibsemMillingWidget import FibsemMillingWidget
+from fibsem import constants
 from PyQt5 import QtWidgets
 from salami.ui.qt import SalamiUI
 
@@ -18,18 +19,20 @@ class SalamiUI(SalamiUI.Ui_MainWindow, QtWidgets.QMainWindow):
         super(SalamiUI, self).__init__()
         self.setupUi(self)
         self.viewer = viewer
+        self.viewer.window._qt_viewer.dockLayerList.setVisible(False)
+        self.viewer.window._qt_viewer.dockLayerControls.setVisible(False)
 
         self.setup_connections()
-
-        self.update_ui()
-
+        
         # reusable components
-        self.image_widget = FibsemImageSettingsWidget()
-        self.movement_widget = FibsemMovementWidget()
+        self.image_widget = FibsemImageSettingsWidget(viewer=self.viewer)
+        self.movement_widget = FibsemMovementWidget(viewer=self.viewer, image_widget=self.image_widget)
+        self.milling_widget = FibsemMillingWidget(viewer=self.viewer, image_widget=self.image_widget)
         
         self.gridLayout_imaging_tab.addWidget(self.image_widget, 0, 0)
         self.gridLayout_movement_tab.addWidget(self.movement_widget, 0, 0)
-
+        self.gridLayout_milling_tab.addWidget(self.milling_widget, 0, 0)
+        
 
     def setup_connections(self):
 
@@ -38,62 +41,36 @@ class SalamiUI(SalamiUI.Ui_MainWindow, QtWidgets.QMainWindow):
     def push_button_clicked(self):
         print("button pushed")
 
-    def update_ui(self):
+        # image settings
+        image_settings: ImageSettings = self.image_widget.get_settings_from_ui()
+        # pattern settings
+        pattern_settings: FibsemPatternSettings = self.milling_widget.get_pattern_settings_from_ui()
 
-        import numpy as np
+        print(image_settings)
+        print(pattern_settings)
 
-        self.eb_image = np.random.random(size=(1024, 1536))
-        self.ib_image = np.random.random(size=(1024, 1536))
+        # general settings 
 
-        self.eb_layer = self.viewer.add_image(self.eb_image, name=BeamType.ELECTRON.name)
-        self.ib_layer = self.viewer.add_image(self.ib_image, name=BeamType.ION.name)
+        n_steps = int(self.spinBox_n_steps.value())
+        milling_step_size = float(self.doubleSpinBox_milling_step_size.value()) * constants.NANO_TO_SI
+        
+        import time
 
+        for step_no in range(n_steps):
+            print(f"------------ \t SLICE {step_no} \t ------------")
+            
+            time.sleep(1)
 
-        self.eb_layer.mouse_double_click_callbacks.append(self._double_click)
-        self.ib_layer.mouse_double_click_callbacks.append(self._double_click)
-       
-        self.eb_layer.mouse_drag_callbacks.append(self._single_click)
-        self.ib_layer.mouse_drag_callbacks.append(self._single_click)
+            print(f"------------ \t ALIGN {step_no} \t ------------")
+            time.sleep(1)
 
+            print(f"------------ \t VIEW  {step_no} \t ------------")
+            time.sleep(1)
 
-        self.viewer.grid.enabled = True
+            # update pattern
+            pattern_settings.centre_y += milling_step_size # TODO: make this work for line too
 
-    # double click to move
-
-    # click on ion to move pattern
-
-    # grid view
-
-    def _double_click(self, layer, event):
-
-        coords = layer.world_to_data(event.position)
-
-        # print(f"event: {event.type}, layer: {layer}, coords: {coords}, ")
-
-        # get the relative coords in the beam image
-        coords, beam_type = ui_utils.get_beam_coords_from_click(coords, self.eb_image)
-
-        if beam_type is None:
-            napari.utils.notifications.show_info(f"Please click inside image to move.")
-            return 
-
-        if beam_type is BeamType.ELECTRON:
-            adorned_image = self.eb_image
-        if beam_type is BeamType.ION:
-            adorned_image = self.ib_image
-
-        print(f"beam_type: {beam_type}, coords: {coords}")
-
-    
-    # TODO: disable single click and double click at the same time?
-    def _single_click(self, layer, event):
-
-        if event.type == "mouse_press":
-
-            coords = layer.world_to_data(event.position)
-
-            # print(f"event: {event.type}, layer: {layer}, coords: {coords}, ")
-
+            # TODO: update ui here too.. multi-thread whole program probs
 
 
 def main():
